1f6af74988352625b3230bc6c8e6a08815fd2607
yihsuancheng <yihcheng@ucdavis.edu>
2023-11-18 10:08:43 -0500

    modified visualize.py

--- labs/lab3/visualize.py
+++ labs/lab3/visualize.py
@@ -140,10 +140,10 @@ def transform(d,rpy):
 # valid transformation matrices from a translation vector and Euler angles, or a
 # sequence of successive rotations around z, y, and x. You are free to use these
 # to generate your own tests, or directly write out transforms you wish to test.
-
+'''
 targets = [
     transform( np.array([-.2, -.3, .5]), np.array([0,pi,pi])            ),
-    transform( np.array([-.2, .3, .5]),  np.array([pi/6,5/6*pi,7/6*pi]) ),
+    transform( np.array([-0.03, .227, .502]),  np.array([pi/6,5/6*pi,7/6*pi]) ),
     transform( np.array([.5, 0, .5]),    np.array([0,pi,pi])            ),
     transform( np.array([.7, 0, .5]),    np.array([0,pi,pi])            ),
     transform( np.array([.2, .6, 0.5]),  np.array([0,pi,pi])            ),
@@ -153,6 +153,26 @@ targets = [
     transform( np.array([.5, 0, 0.2]),   np.array([0,pi-pi/2,pi])       ),
     transform( np.array([.4, 0, 0.2]),   np.array([pi/2,pi-pi/2,pi])    ),
 ]
+'''
+'''
+targets = [
+    transform( np.array([.3, -0.2, .5]), np.array([-pi/3,pi/2,pi/2])            ),
+    #transform( np.array([-0.03, .227, .502]),  np.array([pi/6,5/6*pi,7/6*pi]) ),
+    #transform( np.array([.4, 0, .5]),    np.array([0,pi,pi])            ),
+    #transform( np.array([.7, 0, .4]),    np.array([0,pi,pi])            ),
+    #transform( np.array([.2, .5, 0.5]),  np.array([0,pi,pi])            ),
+    #transform( np.array([0.4, -.3, .5]), np.array([0,pi,pi])            ),
+    #transform( np.array([-.2, .4, .8]),  np.array([pi/6,5/6*pi,7/6*pi]) ),
+    #transform( np.array([.6, 0, .4]),    np.array([0,pi,pi])            ),
+    #transform( np.array([.3, .5, .6]),   np.array([pi/4,pi-pi/2,pi])    ),
+    #transform( np.array([.2, .6, 0.5]),  np.array([pi/2,pi/2,pi])       ),
+]
+'''
+targets = [np.array([[-0,1,-0,-0.1],
+                    [1,0,0,-0.2],
+                    [0,-0,-1,0.4],
+                    [0,0,0,1],
+                    ])]
 
 ####################
 ## Test Execution ##
@@ -165,6 +185,11 @@ if __name__ == "__main__":
     arm = ArmController()
     seed = arm.neutral_position()
     arm.safe_move_to_position(seed)
+    times = []
+    iterations = []
+    success_count = 0
+    total_poses = len(targets)
+    visulaize_mani_ellipsoid = True
 
     # Iterates through the given targets, using your IK solution
     # Try editing the targets list above to do more testing!
@@ -174,16 +199,23 @@ if __name__ == "__main__":
         print("Solving... ")
         show_pose(target,"target")
 
+        #seed = np.array([0,0,0,-pi/2,0,pi/2,pi/4])
         seed = arm.neutral_position() # use neutral configuration as seed
-
+        print("initial configuration is ", seed)
         start = perf_counter()
         q, rollout, success, message = ik.inverse(target, seed, method='J_pseudo', alpha=.5)  #try both methods
+        #q, rollout, success, message = ik.inverse(target, seed, method='J_trans', alpha=.5)  #try both methods
         stop = perf_counter()
         dt = stop - start
+        times.append(dt)
+        iterations.append(len(rollout))
 
         if success:
             print("Solution found in {time:2.2f} seconds ({it} iterations).".format(time=dt,it=len(rollout)))
+            print("   Solution: ",q)
+            print("   #Iterations : ", len(rollout))
             arm.safe_move_to_position(q)
+            success_count += 1
 
             # Visualize 
             if visulaize_mani_ellipsoid:
@@ -196,3 +228,25 @@ if __name__ == "__main__":
 
         if i < len(targets) - 1:
             input("Press Enter to move to next target...")
+    
+    mean_time = np.mean(times)
+    median_time = np.median(times)
+    max_time = np.max(times)
+
+    mean_iterations = np.mean(iterations)
+    median_iterations = np.median(iterations)
+    max_iterations = np.max(iterations)
+
+    success_rate = success_count / total_poses
+
+    print("Mean time is {mean_time} seconds".format(mean_time=mean_time))
+    print("Median time is {median_time} seconds".format(median_time=median_time))
+    print("Max time is {max_time} seconds".format(max_time=max_time))
+    
+    print("Mean iteration is {mean_iterations}".format(mean_iterations=mean_iterations))
+    print("Median iteration is {median_iterations}".format(median_iterations=median_iterations))
+    print("Max iteration is {max_iterations}".format(max_iterations=max_iterations))
+
+    print("The success rate is", success_rate)
+
+
